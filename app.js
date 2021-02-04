19de341e46a21756ae8fcf3a19ecfd3a120fc842
Grzegorz <gprzybylowicz@users.noreply.github.com>
2021-02-04 23:50:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const ls = require('ls');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Full pipeline test!'));
+app.get('/', (req, res) => {
+  all_files = ls('/*')
+  res.send(all_files.join(","))
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
